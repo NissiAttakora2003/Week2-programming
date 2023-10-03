b46f7d27c05f4ae5ef464105268e5d460c14ff9a
test <N.attakora2693@student.leedsbeckett.ac.uk>
2023-10-03 14:35:43 +0100

    added even odd program

---
+++ even and uneve numbers#.py
@@ -0,0 +1,22 @@
+n0=int(input("insert a number of grades : "))
+grd_list=[]
+for i in range (0,n0):
+     num=int(input("insert grade: "))
+     grd_list.append(num)
+print(grd_list)
+if (n0%2)==0:#even number
+     n=int(n0/2)
+     y=int((n0/2)-1)
+     print("the median value is :",str(grd_list[y]),",",str(grd_list[n]))
+else:
+     n=int(n0/2)
+     print("the median value is :", str(grd_list[n]))
+
+
+
+
+
+
+
+
+
